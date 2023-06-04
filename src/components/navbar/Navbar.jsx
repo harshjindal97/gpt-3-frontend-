e8dd146abb6d883b676eb87a68310cd4b4b1fb98
harshjindal97 <hasrh946643@gmail.com>
2023-06-04 22:37:17 +0530

    working on header and navbar responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 import "./navbar.css";
 
 const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className='gpt3__navbar'>
             <div className='gpt3__navbar_links'>
@@ -21,6 +24,29 @@ const Navbar = () => {
             <div className='gpt3__navbar-sign'>
                 <p>Sign in</p>
                 <button className='gpt3__navbar-sing-button'>Sing Up</button>
+            </div>
+            <div className='gpt3__navbar-menu'>
+                {toggleMenu
+                    ? <RiCloseLine color='#fff' size={27} onClick={() => { setToggleMenu(false) }} /> :
+                    <RiMenu3Line color='#fff' size={27} onClick={() => { setToggleMenu(true) }} />
+                }
+                {toggleMenu && (
+                    <div className='gpt3__navbar-menu_container scale-up-center'>
+                        <div className='gpt3__navbar-menu_container-links'>
+                            <p><a href="#home">Home</a></p>
+                            <p><a href="#wgp3">What is gpt</a></p>
+                            <p><a href="#possibility">Open AI</a></p>
+                            <p><a href="#features">Case Studies</a></p>
+                            <p><a href="#blog">Library</a></p>
+
+                            <div className='gpt3__navbar-menu_container-links-sign'>
+                                <p>Sign in</p>
+                                <button className='gpt3__navbar-sing-button'>Sing Up</button>
+                            </div>
+                        </div>
+
+                    </div>
+                )}
             </div>
         </div>
     )
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import "./header.css";
+import aipng from '../../assets/ai.png'
+import people from '../../assets/people.png'
 
 const Header = () => {
     return (
         <div>
-            Header
+            <div className='gpt3_header section_padding' id='home'>
+                <div className='gpt3_header-content'>
+                    <h1 className='gradient-text'>Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+                    <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                    <div className='gpt3_header-content_input'>
+                        <input type="email" placeholder='Your Email Address' />
+                        <button type='button'>Get Started</button>
+                    </div>
+                    <div className='gpt3_header-content_people'>
+                        <img src={people} alt="image" />
+                        <p>1600 people requested access a visit in last 24 hours</p>
+                    </div>
+
+                </div>
+                <div className='gpt3_header-image'>
+                    <img src={aipng} alt="ai" />
+                </div>
+            </div>
         </div>
     )
 }
