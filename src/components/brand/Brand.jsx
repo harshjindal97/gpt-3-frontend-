6021846b67a7cf7e2125a64d5cb0327c3c956de5
harshjindal97 <hasrh946643@gmail.com>
2023-06-08 14:40:12 +0530

    complete header and brand section and working on features and wgpt section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Brand.css";
 
+import { google, slack, atlassian, dropbox, shopify } from './import';
+
 const Brand = () => {
     return (
-        <div>
-            Brand
+        <div className='gpt3_brand'>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={slack} alt="" />
+            </div>
+            <div>
+                <img src={atlassian} alt="" />
+            </div>
+            <div>
+                <img src={dropbox} alt="" />
+            </div>
+            <div>
+                <img src={shopify} alt="" />
+            </div>
         </div>
     )
 }
--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./features.css";
 
-const Features = () => {
+const Features = ({ title, text }) => {
     return (
-        <div>
-            Features
+        <div className='gpt3_feature-container_feature'>
+            <div className='gpt3_feature-container_feature-title'>
+                <div />
+                <h1>
+                    {title}
+                </h1>
+            </div>
+
+            <div className='gpt3_feature-container_feature-text'>
+                {text}
+            </div>
+
         </div>
     )
 }
